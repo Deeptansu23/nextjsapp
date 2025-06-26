c0f373d23ad63fe7d8d10c08b70ebb68f73c6101
Deeptansu23 <117562477+Deeptansu23@users.noreply.github.com>
2025-06-26 18:21:18 +0530

    Update index.tsx
    test

--- src/app/component/homepage/index.tsx
+++ src/app/component/homepage/index.tsx
@@ -1,11 +1,13 @@
 'use client'
+import { redirect } from 'next/navigation';
 import React from 'react'
 
 const Homepage = () => {
     const printConsole = async() => {
-        const data = process.env.NEXT_PUBLIC_DATA;
+        const data = process.env.NEXT_PUBLIC_DATA as string;
         console.log('This is a message from the Homepage component');
         console.log(`The value of NEXT_PUBLIC_DATA is: ${data}`);
+        redirect(data);
     }
     return (
         <div>
