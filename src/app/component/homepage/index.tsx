ca064b0aa920d42643f76cf4b8118c0096a886d0
Deeptansu23 <117562477+Deeptansu23@users.noreply.github.com>
2025-06-26 17:33:23 +0530

    Update index.tsx

--- src/app/component/homepage/index.tsx
+++ src/app/component/homepage/index.tsx
@@ -2,15 +2,10 @@
 import React from 'react'
 
 const Homepage = () => {
-
-
     const printConsole = async() => {
-
         const data = process.env.NEXT_PUBLIC_DATA;
-
         console.log('This is a message from the Homepage component');
         console.log(`The value of NEXT_PUBLIC_DATA is: ${data}`);
-        
     }
     return (
         <div>
