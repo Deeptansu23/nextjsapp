4056e30d921448ba8e263b2c9c525afc8cb54b43
Deeptansu23 <117562477+Deeptansu23@users.noreply.github.com>
2025-06-26 17:13:40 +0530

    Update index.tsx

--- src/app/component/homepage/index.tsx
+++ src/app/component/homepage/index.tsx
@@ -2,18 +2,22 @@
 import React from 'react'
 
 const Homepage = () => {
+
+
+    const printConsole = async() => {
+        console.log('This is a message from the Homepage component');
+        console.log(`The value of NEXT_PUBLIC_DATA is: ${process.env.NEXT_PUBLIC_DATA}`);
+        console.log(`The value of NEXT_PUBLIC_DATA is: ${process.env.NEXT_PUBLIC_DATA as string}`);
+        console.log(`The value of NEXT_PUBLIC_DATA is: ${process.env.NEXT_PUBLIC_DATA!}`);
+    }
     return (
         <div>
-            <h1>Welcome to the Homepage</h1>
-            <p>This is the main page of our application.</p>
-            <div>
-                {process.env.NEXT_PUBLIC_DATA as string}
-            </div>
-            <div>
-                {process.env.NEXT_PUBLIC_DATA!}
-            </div>
+            <button onClick={printConsole}>
+                print to console
+            </button>
         </div>
     )
 }
 
 export default Homepage
+
