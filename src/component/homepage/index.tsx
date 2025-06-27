98f935cb921366be59eb43603ad10bf0c98e424b
Deeptansu <deeptansu.sahu@prodevans.com>
2025-06-27 11:09:33 +0530

    testing

---
+++ src/component/homepage/index.tsx
@@ -0,0 +1,22 @@
+'use client'
+import { redirect } from 'next/navigation';
+import React from 'react'
+
+const Homepage = () => {
+
+
+    const printConsole = async () => {
+        console.log('This is a message from the Homepage component');
+        console.log(process.env.NEXT_PUBLIC_DATA);
+
+    }
+    return (
+        <div>
+            <button onClick={printConsole}>
+                print to console
+            </button>
+        </div>
+    )
+}
+
+export default Homepage
