b5aa697aa38be98df28c17ef14ed63f1c2fb0ab6
deeptansu.sahu@prodevans.com <deeptansu.sahu@prodevans.com>
2025-11-11 11:33:36 +0530

    add multile script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({
           src="https://chat-cdn.qweryai.in/widget-dev-min.js?id=395136cc-8cb1-4d4f-949c-9333f5ba176c"
           strategy="afterInteractive"
         />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossOrigin="anonymous"></script>
         {children}
       </body>
     </html>
